86603ab0c3a82ab0dd47f5efff086b29b3762a1b
Anubhav Baghel <code.anubhavbaghel@gmail.com>
2026-04-02 16:51:57 +0530

    Fix PHPMailer require paths to match PHPMailer-master folder

--- send-mail.php
+++ send-mail.php
@@ -2,9 +2,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
 
 header('Content-Type: application/json');
 
